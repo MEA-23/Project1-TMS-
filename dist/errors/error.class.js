20d7063e753916e5ab2967ce0f34674363f19619
MEA-23 <77838294+MEA-23@users.noreply.github.com>
2024-10-27 10:08:42 +0300

    Delete dist directory

--- dist/errors/error.class.js
+++
@@ -1,8 +0,0 @@
-export class ErrorHandlerClass {
-    constructor(message, statusCode, stack, data) {
-        this.message = message;
-        this.statusCode = statusCode;
-        this.stack = stack || null;
-        this.data = data || "Error";
-    }
-}
--- dist/middlewares/notFound.middleware.js
+++
@@ -1,4 +0,0 @@
-import { ErrorHandlerClass } from "../errors/error.class";
-export const notFoundMiddleware = (req, res, next) => {
-    next(new ErrorHandlerClass("Not Found", 404, null, "API DATA"));
-};
